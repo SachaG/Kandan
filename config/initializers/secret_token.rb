79d6ce1e53c836c4cfefc1a120828f69601476e4
Sacha Greif <sacha357@gmail.com>
2011-04-29 13:03:21 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kandan::Application.config.secret_token = 'ec1bb0a04fe013a256d8bc494e948ea9e67c2246bae516903be73ed0f37190286637632802d8589f8ef28eac5eb18fa43e80b35028885626a72e65c38824992d'
